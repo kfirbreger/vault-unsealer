b076035e3c4383b62ad73ad8f456b68a7ec89d42
Kfir Breger <kfir@conopa.co>
2019-02-27 16:37:02 +0100

    Updating unsealer

--- internal/unseal.go
+++ internal/unseal.go
@@ -1,6 +1,7 @@
 package unsealer
 
 import (
+    "encoding/json"
     "fmt"
     "http"
     "time"
@@ -8,7 +9,7 @@ import (
 
 
 type unsealparams struct {
-    Key *string
+    Key *string `json:"key"`
     reset bool
     migrate bool
 }
@@ -32,7 +33,26 @@ func checkStatus(url) (bool, error) {
     return unsealed, err
 }
 
-func Unsealer(vault *Vault, instance *Instance, status chan int, keys *[]string) {
+func unsealCall(url string, payload unsealparams) (status int, err error) {
+    /* Making an unseal call for an instance */
+    // Converting the payload to a byte array
+    jsonBytesPayload := []byte(json.Marshal(payload))
+    req, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonBytesPayload)
+    
+    // Cleaning key from memory
+    jsonBytesPayload = nil
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    // Making sure body is closed
+    defer resp.Body.Close()
+    if err != nil {
+        return -1, err
+    }
+    return resp.StatusCode, err
+}
+
+func instanceMonitor(vault *Vault, instance *Instance, status chan int, keys *[]string) {
     // Using status to send status update to main process
     // Creating the urls to use
     statusUrl := *vault.protocl + "://" + *instance.Domain + *vault.StatusPath
@@ -42,9 +62,14 @@ func Unsealer(vault *Vault, instance *Instance, status chan int, keys *[]string)
         status, err := checkStatus(statusUrl)
         if err != nil {
             fmt.Println(err)
-        } else {
-            if !unsealed {
-                // This instance is sealed and needs to be unsealed
-                for i := 0;i < len(keys); i++ {
-                    
+        } else if !unsealed {
+            // This instance is sealed and needs to be unsealed
+            for i := 0;i < len(keys); i++ {
+                // Create payload
+                // Do a put (post?) request
+            }
+        }
+        time.Sleep(*vault.CheckInterval * time.Millisecond)            
+    }
+}
 
