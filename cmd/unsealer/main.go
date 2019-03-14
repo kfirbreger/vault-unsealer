de99764b90adcd822801adb7665a1658b09fadb2
Kfir Breger <kfir@conopa.co>
2019-03-14 16:21:29 +0100

    Moving responsibilities to where they belong and setting the main work flow in main

--- cmd/unsealer/main.go
+++ cmd/unsealer/main.go
@@ -1,62 +1,53 @@
 package main
 
 import (
-    "bufio"
-    "flag"
-    "fmt"
-    "http"
-    "os"
-    "strings"
-    "time"
-
-    "github.com/kfirbreger/vault-unsealer/vaultUnsealer"
+    "sync"
+    "github.com/kfirbreger/vault-unsealer/unsealer"
 )
 
-
-// Setting default unseal key count
-const unsealKeyCount = 3
-const millisecondCheckDelay = 100  // How long to wait before vault status checks
-
-func getKeyCount() int {
-    keyCount := flag.Int("unsealing-keys", unsealKeyCount, "The number of keys that are required to unseal the vault. You will be prompt for them after this")
-    flag.Parse()
-    return *keyCount
-}
-
-func readKeys(keyCount int) []string {
-    // Save the unsealing keys in a slice
-    // Need to move it to memguard so its safe in memory
-    var keys []string
-    reader := bufio.NewReader(os.Stdin)
-    for i:= 1; i < keyCount + 1; i++ {
-        fmt.Printf("Unsealing key %d: ", i)
-        text, _ := reader.ReadString('\n')
-        // convert CRLF to LF
-        text = strings.Replace(text, "\n", "", -1)
-        keys = append(keys, text)
-    }
-    return keys
-}
-
-func checkVaultStatus(url string) {
-    // Checks what the vault status is
-    // Once an http error is returned
-    // The vault is sealed and needs to be unsealed
-    for {
-        _, err := ttp.get(url)
-        if err != nil {
-            fmt.Println("Vault is sealed. Time to unseal")
-        }
-        time.Sleep(millisecondCheckDelay * time.Millisecond)
-    }
-}
-
-func unsealVault(unsealKeys *[]string, vaultUrl *string) {
-
 func main() {
-    // var key_count int
-    keyCount := getKeyCount()
-    fmt.Println("Unsealing key count: ", keyCount)
-    keys := readKeys(keyCount)
-    fmt.Println(keys)
-}
+    // Load config
+    conf := unsealer.LoadConfiguration()
+    // Update with command line arguments
+    // @TODO add command line arguments handling
+
+    // Retrieve unseal keys, and put them
+    // in a memguard array. Only one copy is
+    // allowed to exist to all refrences will
+    // be done via pointers to prevent gc from
+    // moving the keys around
+    keys := unsealer.GetUnsealKeys()
+
+    // Start the following:
+    // 1. Channels
+    // 2. Checker workers
+    // 3. Unseal workers
+    // 4. Status check generators
+    checkerQueue := make(chan unsealer.StatusCheckRequest, 10) // TODO make this service count * something
+    unsealQueue := make(chan unsealer.UnsealRequest, 20)  // TODO same here
+    logChan := make(chan string, 10)
+
+    // Creating Cehcker workers
+    var checkers = [conf.Workers.StatusCheckCount]Checker
+    var unsealers = [conf.Workers.UnsealCount]Unsealer
+    var loggers = [conf.Workers.LoggingCount]string  // TODO add loggers
+
+    for i := 0; i < len(checkers): i++ {
+        // Creating checkers
+        c := NewChecker(i, checkerQueue, unsealQueue, logChan)
+        *c.Start()
+        checkers[i] = c
+    }
+
+    // Creating unseal params
+    up := *unsealer.unsealparams{*keys, false, false}
+    // Creating unsealer workers
+    for i := 0; i < len(unsealQueue), i++ {
+        u = NewUnsealer(i, unsealQueue, logChan, up)
+        *u.Start()
+        unsealers[i] = u
+    }
+
+    // Creating the Status check generators
+    for {
+
