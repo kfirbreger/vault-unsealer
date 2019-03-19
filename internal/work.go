7e8f4d65d31a3265be54a1497f7d93a504836ef5
Kfir Breger <kfir@conopa.co>
2019-03-19 16:11:49 +0100

    Removing unneeded code

--- internal/work.go
+++ internal/work.go
@@ -1,16 +1,5 @@
 package unsealer
 
-const STATUSCHECK = 100
-const UNSEALVAULT = 101
-
-// this can be removed later
-type WorkRequest struct {
-    Name string
-    Url string
-    Action int
-    Params map[string]string
-}
-
 type StatusCheckRequest struct {
     Name string
     Url string
