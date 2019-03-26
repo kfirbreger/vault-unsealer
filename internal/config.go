9007f5e7490846a2d77ffc73870a588ef54d6f42
Kfir Breger <kfir@conopa.co>
2019-03-26 17:15:30 +0100

    [#5] Accepting config flag to set the configuration file

--- internal/config.go
+++ internal/config.go
@@ -3,6 +3,9 @@ package internal
 import (
 	"flag"
 	"fmt"
+    "log"
+    "os/user"
+    "path/filepath"
 
 	"github.com/BurntSushi/toml"
 )
@@ -40,6 +43,7 @@ func Load(filepath string, s *Service) {
 }
 
 type CliParams struct {
+    ConfigFile     string
 	UnsealKeyCount int
 	StatusPath     string
 	UnsealPath     string
@@ -51,6 +55,7 @@ type CliParams struct {
 func getCliParams() CliParams {
 	var params CliParams
 
+    params.ConfigFile = *flag.String("config", "", "Specify the configuration file to use")
 	params.UnsealKeyCount = *flag.Int("unsealing-keys", 0, "The number of keys that are required to unseal the vault. You will be prompt for them after this")
 	params.StatusPath = *flag.String("status-path", "", "Give a custom status check path")
 	params.UnsealPath = *flag.String("unseal-path", "", "Give a custom unseal path")
@@ -69,7 +74,38 @@ func updateConfig(serv *Service, params *CliParams) {
 
 func LoadConfiguration() *Service {
 	var conf Service
-	Load("./config.toml", &conf)
+    cliParams := getCliParams()
+    // Reading the config file
+    configFile := "./config.toml"
+    if cliParams.ConfigFile {
+        if _, err := os.Stat(cliParams.ConfigFile); err == nil {
+            // There is a file with that name, so use it as a config file
+            configFile = expand(cliParams.ConfigFile)
+        } else if os.IsNotExist(err) {
+            log.Fatalf("The file %s does not seem to exist", cliParams.ConfigFile)
+        } else {
+            // Something is wrong, display the error and terminate
+            log.Fatal(err)
+        }
+    }
+
+	Load(configFile, &conf)
 	// TODO add cli params
 	return &conf
+}
+
+func expand(path string) (string, error) {
+    /**
+    Expand the directory to be absolute instead of relative path
+    as using ~ will not always work when opening files
+    */
+    if len(path) == 0 || path[0] != '~' {
+        return path, nil
+    }
+
+    usr, err := user.Current()
+    if err != nil {
+        return "", err
+    }
+    return filepath.Join(usr.HomeDir, path[1:]), nil
 }
