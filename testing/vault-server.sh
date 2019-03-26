46d9f29878a2a363b48f608e3932c5c578be73b8
Kfir Breger <kfir@conopa.co>
2019-03-26 10:04:27 +0100

    Adding init to the script and puttig the unsealing keys into a file to be used in testing

--- testing/vault-server.sh
+++ testing/vault-server.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 docker run --rm --name vault -d -p 8200:8200 --cap-add=IPC_LOCK -e 'VAULT_LOCAL_CONFIG={"backend": {"file": {"path": "/vault/file"}}, "listener": { "tcp": {"address": "0.0.0.0:8200", "tls_disable": "1"}}}' -e 'VAULT_ADDR=http://127.0.0.1:8200' vault server
+
+# Giving docker 2 seconds to start it all up
+sleep 2
+
+# Init the server. Get the unseal keys only and save them to a text file
+docker exec vault vault operator init | grep "Unseal Key" | awk '{print $4}' > keys.txt
+
+
