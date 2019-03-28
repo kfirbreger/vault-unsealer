f812ee5f418879e6ea8df7da2e68fcf5279e712e
Kfir Breger <kfirbreger@users.noreply.github.com>
2019-03-28 17:05:33 +0100

    Adding time test

--- internal/generator_test.go
+++ internal/generator_test.go
@@ -5,6 +5,10 @@ import (
     "time"
 )
 
+
+const ALLOWEDTIMEDIFFERENCETHRESHOLD 10
+
+
 func TestUrlCreatedCorrectly(t *testing.T) {
     sc := make(chan StatusCheckRequest, 10)
     domain := "localhost:1234"
@@ -19,4 +23,27 @@ func TestUrlCreatedCorrectly(t *testing.T) {
     if msg.Url != "https://localhost:1234/1/2/3" {
         t.Errorf("Expected url to be \"https://localhost:1234/1/2/3\" but got %s instead\n", msg.Url)
     }
+}
+
+func TestStatusCheckInterval(t *testing.T) {
+    sc := make(chan StatusCheckRequest, 10)
+    domain := "localhost:1234"
+    protocol := "https"
+    stsPth := "1/2/3"
+    interval := 100
+    
+    go GenerateChecks(sc, domain, protocol, stsPth, interval)
+    // Waiting for a message
+    msg <- sc
+    startTime := timestamp()
+    
+    msg <- sc
+    endTime := timestamp()
+    
+    if (endTime - startTime - interval) < ALLOWEDTIMEDIFFERENCETHRESHOLD
+}
+
+func timestamp() int64 {
+    // Generate an int64 timestamp
+    return time.Now().UnixNano() / int64(time.Millisecond)
 }
