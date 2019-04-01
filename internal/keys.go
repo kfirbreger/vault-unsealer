0f3320fa11a162299630f3f213f604c9d051c713
Kfir Breger <kfir@conopa.co>
2019-04-01 16:48:45 +0200

    Removing declaring the capacity of the key slice

--- internal/keys.go
+++ internal/keys.go
@@ -12,7 +12,7 @@ import (
 
 func GetUnsealKeys(keyCount int, configKeys []string) []*memguard.LockedBuffer {
 	// Save the unsealing keys in a slice
-	keys := make([]*memguard.LockedBuffer, 0, keyCount)
+	keys := make([]*memguard.LockedBuffer, 0)
 	readKeys := true
 	// Checking if keys are given through config
 	// and if they are, skip reading from user
