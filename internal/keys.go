ae5433520c22dd7a4726d53e0c08df01408b3404
Kfir Breger <kfir@conopa.co>
2019-04-12 19:48:08 +0200

    Improving key error reporting

--- internal/keys.go
+++ internal/keys.go
@@ -12,29 +12,28 @@ import (
 
 func GetUnsealKeys(keyCount int, configKeys []string) []*memguard.LockedBuffer {
 	// Save the unsealing keys in a slice
-	keys := make([]*memguard.LockedBuffer, 0, keyCount)
+    keys := make([]*memguard.LockedBuffer, 0, keyCount)
 	readKeys := true
 	// Checking if keys are given through config
 	// and if they are, skip reading from user
 	if len(configKeys) > 0 {
 		readKeys = false
+        log.Println("Keys given via CLI.")
 	}
 	reader := bufio.NewReader(os.Stdin)
 	var singleKey = make([]byte, 0)
 	var err error
 	for i := 0; i < keyCount; i++ {
 		if readKeys {
-			fmt.Printf("Unsealing key %d: \n", i)
 			singleKey, err = reader.ReadBytes('\n')
 			if err != nil {
-				fmt.Println(err)
+				log.Fatal(err)
 			}
 		} else {
 			singleKey = []byte(configKeys[i])
 		}
 		// convert CRLF to LF
 		singleKey = bytes.TrimSpace(singleKey)
-		fmt.Println(singleKey)
 		membuf, err := memguard.NewImmutableFromBytes(singleKey)
 		if err != nil {
 			log.Fatal("Eror creating memory safe storage", err)
