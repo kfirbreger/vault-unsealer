032e66edce7ed574e0b8743999d24351da083573
Kfir Breger <kfir@conopa.co>
2019-04-12 20:14:22 +0200

    Testing in HA mode

--- testing/compose/test-ha.sh
+++ testing/compose/test-ha.sh
@@ -14,8 +14,30 @@ docker exec unsealer_vault-0_1 vault operator init | grep "Unseal Key" | awk '{p
 # Starting the unsealer
 # This should lead to the vaults being unsealed
 docker build -f Dockerfile ../../ --tag unsealer
-docker run --rm --network=safe --cap-add=IPC_LOCK unsealer
+docker run --name=unsealer -d --rm --network=safe --cap-add=IPC_LOCK unsealer
 
-# Cleaning up
-#rm keys.txt
+echo "Giving everything a minute to stabilize"
+sleep 60
 
+echo "Showing unsealer logs:"
+echo "======================"
+docker logs unsealer
+
+echo "Showing vault-0 status"
+docker-compose --project-name unsealer exec vault-0 vault status
+
+echo "Showing vault-1 status"
+docker-compose --project-name unsealer exec vault-1 vault status
+
+echo "Restarting a vault server"
+docker-compose --project-name unsealer restart vault-1
+
+echo "Giving it 10 seconds"
+sleep 10
+
+echo "Showing vault-1 status"
+docker-compose --project-name unsealer exec vault-1 vault status
+
+echo "Cleaning it all up"
+
+./cleanup.sh
