3373bbcf78870c8455f81f1afe1b27e529a650b1
Kfir Breger <kfir@conopa.co>
2019-05-01 13:30:19 +0200

    Golint

--- internal/work.go
+++ internal/work.go
@@ -1,11 +1,13 @@
 package internal
 
+// StatusCheckRequest - The type for a status check request
 type StatusCheckRequest struct {
 	Name   string
 	Url    string
 	Domain string
 }
 
+// UnsealRequest - The type for unsealing requests
 type UnsealRequest struct {
 	Name      string
 	Url       string
